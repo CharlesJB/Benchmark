b153db1495bfd8e5eecdf1c85cb4e4cfe20f5a22
CharlesJB <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2012-02-21 15:44:57 -0500

    Corrected mistake in arguments order

--- Templates/SISSR_Analysis_Template.sh
+++ Templates/SISSR_Analysis_Template.sh
@@ -2,4 +2,4 @@
 
 source /home/cjbparlant/git-clones/NGS-Pipelines/LoadModules.sh
 
-/home/cjbparlant/git-clones/Benchmark/SISSR_Analysis.sh __PROCESSOR__ __FILETYPE__ __OUTPUT__ __OPTIONS__  __TREATMENTFILE__ __CONTROLFILE__
+/home/cjbparlant/git-clones/Benchmark/SISSR_Analysis.sh __PROCESSOR__ __OUTPUT__ __FILETYPE__ __OPTIONS__  __TREATMENTFILE__ __CONTROLFILE__
