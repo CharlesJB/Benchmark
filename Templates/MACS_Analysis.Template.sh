061b6cb07e2ca5f15c6994a1af776ac3fbf51178
CharlesJB <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2012-02-21 15:45:15 -0500

    New template file

---
+++ Templates/MACS_Analysis.Template.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /home/cjbparlant/git-clones/NGS-Pipelines/LoadModules.sh
+
+/home/cjbparlant/git-clones/Benchmark/SISSR_Analysis.sh __PROCESSOR__ __OUTPUT__ __FILETYPE__ __OPTIONS__  __TREATMENTFILE__ __CONTROLFILE__
