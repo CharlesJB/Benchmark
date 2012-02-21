1c30bb5fe35bc2ab492d21ab784d9a6e6a1ba831
CharlesJB <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2012-02-21 15:08:20 -0500

    First working version of MACS_Analysis.sh

---
+++ MACS_Analysis.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# author: Charles Joly Beauparlant
+# date: 2012-02-21
+# based on BlueTsunami: github.com/sebhtml/NGS-Pipelines/blob/master/BlueTsunami
+
+source $DARK_FISH_TECHNOLOGY
+source $BENCHMARKTOOLS
+
+# Process arguments
+# The arguments must not be absolute paths.
+processors=$1
+output=$2
+fileFormat=$3
+options=$4
+
+if [ $fileFormat = "txt" ]
+then
+	fileName=$5	
+	BenchmarkTools_extractFileInfos $fileName
+else
+	fileFormats[0]=$fileFormat
+	treatmentFiles[0]=$5
+	controlFiles[0]=$6
+fi
+
+# Prepare analysis
+mkdir $output
+cd $output
+
+DarkFishTechnology_initializeDirectory
+DarkFishTechnology_runCommand 0 "macs14 --version &> meta/macs.version"
+
+# Prepare samples
+BenchmarkTools_prepareSamples
+
+# Do the actual analysis
+BenchmarkTools_MACS_Analysis
